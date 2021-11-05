7a1e167421685e425cf6dbd9d3438a670ebb8015
degante95 <degantecarlos@gmail.com>
2021-11-05 00:29:28 -0500

    fix

--- src/info.js
+++ src/info.js
@@ -7,7 +7,8 @@ const help = String.raw`${usage}
 
   Options:
 
-  [CR|LF|CRLF]: Desired line ending 
+  [CR|LF|CRLF]: Desired line ending
+  
       CR is a carriage return character \r (rare)
       LF is a line feed character \n (common on Unix and macOS)
       CRLF is a combined ending \r\n (common on Windows)
