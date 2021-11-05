34f18af7eaa79b9179390b09fa8467d221c2a87b
degante95 <degantecarlos@gmail.com>
2021-11-05 00:38:03 -0500

    fixes

--- src/info.js
+++ src/info.js
@@ -8,7 +8,7 @@ Converts text between newline (\n), carriage return (\r), and combined
 Options:
 
 [CR|LF|CRLF]: Desired line ending
-  
+
     CR is a carriage return character \r (rare)
     LF is a line feed character \n (common on Unix and macOS)
     CRLF is a combined ending \r\n (common on Windows)
